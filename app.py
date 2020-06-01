75845d657c4a59c9acc1afdc8123c29e47473237
g0utham15 <dsgr369@gmail.com>
2020-06-01 17:19:33 +0530

    intial commit

---
+++ app.py
@@ -0,0 +1,70 @@
+import dash
+import dash_auth
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from flask import request
+
+
+VALID_USERNAME_PASSWORD_PAIRS = [
+    ['admin', 'admin'],
+    ['doctor', 'doctor']
+]
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', "assets\main.css"]
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+auth = dash_auth.BasicAuth(
+    app,
+    VALID_USERNAME_PASSWORD_PAIRS
+)
+
+app.layout = html.Div([
+    html.H1("Health SCADA", style={'textAlign': 'center'}),
+    html.Div([
+        html.Div([
+        html.Button("Bed-1", className='bed'),
+        html.Button('Bed-2', className='bed'),
+        html.Button('Bed-3', className='bed'),
+        html.Button('Bed-3', className='bed')
+        ],
+        className='room',
+        style = {'width': '100%', 'display': 'flex', 'align-items': 'center', 'justify-content': 'center','height': 250, "border":"2px black solid"},),
+        html.Div([
+            html.Button("Bed-4", className='bed'),
+            html.Button("Bed-4", className='bed'),
+            html.Button("Bed-4", className='bed'),
+            html.Button('Bed-3', className='bed')
+        ],
+        className='room',
+        style = {'width': '100%', 'display': 'flex', 'align-items': 'center', 'justify-content': 'center','height': 250,"border":"2px black solid"},),
+        ], 
+        className='column',
+    ),
+    html.Div([
+            html.Div([
+            html.Button("Bed-1", className='bed'),
+            html.Button('Bed-2', className='bed'),
+            html.Button('Bed-3', className='bed'),
+            html.Button('Bed-3', className='bed')
+        ],
+        className='room',
+        style = {'width': '100%', 'display': 'flex', 'align-items': 'center', 'justify-content': 'center','height': 250,"border":"2px black solid"}),
+        html.Div([
+            html.Button("Bed-4", className='bed'),
+            html.Button("Bed-4", className='bed'),
+            html.Button("Bed-4", className='bed'),
+            html.Button('Bed-3', className='bed')
+        ],
+        className='room',
+        style = {'width': '100%', 'display': 'flex', 'align-items': 'center', 'justify-content': 'center','height': 250, "border":"2px black solid"},),
+        ], 
+        className='column',
+    ),      
+])
+
+
+app.scripts.config.serve_locally = True
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
