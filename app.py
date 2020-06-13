b3c51efe37d6ff3ebd359d21e8016ab714cb689b
g0utham15 <dsgr369@gmail.com>
2020-06-13 23:47:44 +0530

    optimised code

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 import dash
 import dash_html_components as html
-from dash.dependencies import Input, Output
-from flask import request
-
-
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', "assets\main.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
 
 app.layout = html.Div([
     html.H1("Health SCADA", style={'textAlign': 'center'}),
@@ -34,9 +29,5 @@ app.layout = html.Div([
             ),
         ])
         
-
-
-
-
 if __name__ == '__main__':
     app.run_server()
